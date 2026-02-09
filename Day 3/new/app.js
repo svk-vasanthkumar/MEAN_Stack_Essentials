ae38345f0afba11d92587eac2477a67fb5c83d66
svk-vasanthkumar <vasanth636807@gmail.com>
2026-02-09 21:54:12 +0530

    modify a styles

--- Day 3/new/app.js
+++ Day 3/new/app.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var routing = require('./routing.js');
-app.use ('/',routing);
+
+// Serve static assets (CSS, images) from the current folder
+app.use(express.static(__dirname));
+
+// Route handlers
+app.use('/', routing);
+
+// Start server
 app.listen(5000);
-console.log("server is running");
+console.log('server is running on http://localhost:5000');
